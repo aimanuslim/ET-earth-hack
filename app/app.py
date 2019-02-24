e9ff331f7e6bc608c46d5f60b6edb6bcdd7876e0
Hafiz Azmi <hafizuddintgazmi@gmail.com>
2019-02-24 11:55:47 +0800

    dropdown

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
@@ -14,7 +15,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 input_col_labels = ['General', 'Resistivity', 'Density', 'Measured DTC', 'Predicted DTC', 'Error']
 
 input_col_curves = [
-    ['GR', 'BS', 'CALI', 'SP'],
+    ['GR', 'BS', 'CALI'],
     ['RESS', 'RESM', 'RESD'],
     ['DENS', 'NEUT', 'PEF'],
     # ['DTC_act'],
@@ -67,7 +68,25 @@ def dictdata(filepath):
             curvedict[curve] = (df[curve].tolist(),df['DEPT'].tolist())
     return curvedict
 
-data = dictdata('../data/prep/Cheal-B8_Clean.csv')
+def clean():
+    curvedict = {}
+    df1 = pd.read_csv('../clean/Cheal-A10_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df2 = pd.read_csv('../clean/Cheal-A11_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df3 = pd.read_csv('../clean/Cheal-A12_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df4 = pd.read_csv('../clean/Cheal-B8_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df5 = pd.read_csv('../clean/Cheal-C3_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df6 = pd.read_csv('../clean/Cheal-C4_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df7 = pd.read_csv('../clean/Cheal-G1_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df8 = pd.read_csv('../clean/Cheal-G2_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df9 = pd.read_csv('../clean/Cheal-G3_Clean.csv').replace('-9999',np.NaN).iloc[1:]
+    df = pd.concat([df1,df2,df3,df4,df5,df6,df7,df8,df9])
+    for curve in list(df):
+        if curve != 'DEPT':
+            curvedict[curve] = (df[curve].tolist(),df['DEPT'].tolist())
+    return curvedict
+
+data = clean()
+# data = dictdata('../data/prep/Cheal-B8_Clean.csv')
 # data = load_csv()
 # output_data = load_dtc()
 
@@ -140,17 +159,17 @@ app.layout = html.Div([
         dcc.Dropdown(
             id='my-dropdown',
             options=[
-                {'label': 'Cheal-A10', 'value': '../data/prep/Cheal-A10_Clean.csv'},
-                {'label': 'Cheal-A11', 'value': '../data/prep/Cheal-A11_Clean.csv'},
-                {'label': 'Cheal-A12', 'value': '../data/prep/Cheal-A12_Clean.csv'},
-                {'label': 'Cheal-B8', 'value': '../data/prep/Cheal-B8_Clean.csv'},
-                {'label': 'Cheal-C3', 'value': '../data/prep/Cheal-C3_Clean.csv'},
-                {'label': 'Cheal-C4', 'value': '../data/prep/Cheal-C4_Clean.csv'},
-                {'label': 'Cheal-G1', 'value': '../data/prep/Cheal-G1_Clean.csv'},
-                {'label': 'Cheal-G2', 'value': '../data/prep/Cheal-G2_Clean.csv'},
-                {'label': 'Cheal-G3', 'value': '../data/prep/Cheal-G3_Clean.csv'}
+                {'label': 'Cheal-A10', 'value': 'Cheal-A10'},
+                {'label': 'Cheal-A11', 'value': 'Cheal-A11'},
+                {'label': 'Cheal-A12', 'value': 'Cheal-A12'},
+                {'label': 'Cheal-B8', 'value': 'Cheal-B8'},
+                {'label': 'Cheal-C3', 'value': 'Cheal-C3'},
+                {'label': 'Cheal-C4', 'value': 'Cheal-C4'},
+                {'label': 'Cheal-G1', 'value': 'Cheal-G1'},
+                {'label': 'Cheal-G2', 'value': 'Cheal-G2'},
+                {'label': 'Cheal-G3', 'value': 'Cheal-G3'}
             ],
-        value='../data/prep/Cheal-B8_Clean.csv'
+        value='Cheal-A10'
         ),
     ]),
     html.Div([
@@ -165,6 +184,8 @@ app.layout = html.Div([
     ])
 ])
 
+@app.callback(Output('my-graph', 'figure'),
+              [Input('my-dropdown', 'value')])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
