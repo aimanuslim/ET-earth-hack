c8ae3bdfe5a83bd53a723dd0a777fd574cbe3a01
Aiman Uslim <aimanuslim@gmail.com>
2019-02-25 11:01:53 +0800

    Fixed bugs, ready for deployment

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 input_col_labels = ['General', 'Resistivity', 'Density', 'DTC Comparison']
@@ -25,33 +24,39 @@ input_col_curves = [
     # ['Error']
 ]
 
+wellnames = [
+    'A10',
+    'A11',
+    'A12',
+    'B8',
+    'C3',
+    'C4',
+    'G1',
+    'G2',
+    'G3',
+]
 
-error_curve_name = 'Error curve'
+algorithms = [
+    'Gradient boosting',
+    'Linear regression',
+    'Random forrest',
+]
 
 
-def load_csv():
-    data = {}
-    for sbp in input_col_curves:
-        for label in sbp:
-            start_depth = np.random.randint(0, 200)
-            end_depth = np.random.randint(start_depth, 200)
-            data[label] = (
-                np.random.random(end_depth-start_depth)*0.5+np.random.randint(1,3),
-                np.arange(start_depth,end_depth),
-            )
-    return data
+def load_all_data():
+    well_dict = {}
+    for well in wellnames:
+        algo_dict = {}
+        for algo in algorithms:
+            respath = '../Results/Cheal-' + well + '/' + algo + '.csv'
+            algo_dict[algo] = readres(respath)
+        in_out_dict = {}
+        path = '../clean/Cheal-' + well + '_Clean.csv'
+        in_out_dict['in'] = dictdata(path)
+        in_out_dict['out'] = algo_dict
+        well_dict[well] = in_out_dict
+    return well_dict
 
-# def load_dtc():
-#     data = {}
-#     for sbp in output_col_curves:
-#         for label in sbp:
-#             start_depth = np.random.randint(0, 200)
-#             end_depth = np.random.randint(start_depth, 200)
-#             data[label] = (
-#                 np.random.random(end_depth - start_depth) * 0.5 + np.random.randint(1, 3),
-#                 np.arange(start_depth, end_depth),
-#             )
-#     return data
 
 def get_curve(xdata, ydata, dataname, tn):
     return go.Scatter(
@@ -62,7 +67,6 @@ def get_curve(xdata, ydata, dataname, tn):
         hoverinfo='x+name',
         xaxis='x' + str(tn)
     )
-
 
 
 def dictdata(filepath):
@@ -79,123 +83,17 @@ def readres(filepath):
     return pred_data
 
 
-# def clean():
-#     curvedict = {}
-#     df1 = pd.read_csv('../clean/Cheal-A10_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df2 = pd.read_csv('../clean/Cheal-A11_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df3 = pd.read_csv('../clean/Cheal-A12_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df4 = pd.read_csv('../clean/Cheal-B8_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df5 = pd.read_csv('../clean/Cheal-C3_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df6 = pd.read_csv('../clean/Cheal-C4_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df7 = pd.read_csv('../clean/Cheal-G1_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df8 = pd.read_csv('../clean/Cheal-G2_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df9 = pd.read_csv('../clean/Cheal-G3_Clean.csv').replace('-9999',np.NaN).iloc[1:]
-#     df = pd.concat([df1,df2,df3,df4,df5,df6,df7,df8,df9])
-#     for curve in list(df):
-#         if curve != 'DEPT':
-#             curvedict[curve] = (df[curve].tolist(),df['DEPT'].tolist())
-#     return curvedict
 
-# def newdict():
-#     newcurvedict = {}
-#     oripath = "../data/clean"
-#     for root, dirs, files in os.walk(oripath):
-#         for file in files:
-#             if file.endswith(".csv"):
-#                 laspath = os.path.join(root, file)
-#                 curvedict = {}
-#                 df = pd.read_csv(laspath).replace('-9999',np.NaN).iloc[1:]
-#                 name = df['wellName'][1]
-#                 for curve in list(df):
-#                     if curve != 'DEPT':
-#                         curvedict[curve] = (df[curve].tolist(),df['DEPT'].tolist())
-#                 newcurvedict[name] = curvedict[curve]
-#     return newcurvedict
-
-# data = newdict()
-# data = clean()
-# data = dictdata('../data/prep/Cheal-B8_Clean.csv')
-# data = load_csv()
-# output_data = load_dtc()
-
-# fig_out = tools.make_subplots(rows=1, cols=len(output_col_labels),
-#                                 subplot_titles=tuple(output_col_labels),
-#                                 shared_yaxes=True
-#                               )
 fig = tools.make_subplots(rows=1, cols=len(input_col_labels),
                         subplot_titles=tuple(input_col_labels),
                         shared_yaxes=True
                           )
-# col_idx = 1
-#
-# trace_num = 1
-# for sbp in input_col_curves:
-#     for label in sbp:
-#         fig.add_trace(get_curve(data[label][0], data[label][1], label, trace_num), 1, col_idx)
-#         trace_num += 1
-#     col_idx += 1
 
-# col_idx = 1
-# for sbp in output_col_curves:
-#     for label in sbp:
-#         fig_out.add_trace(get_curve(output_data[label][0], output_data[label][1], label), 1, col_idx)
-#     col_idx += 1
+well_dict = load_all_data()
 
-# fig['layout']['xaxis2'].update(
-#     type='log',
-#     # title='x2dgdsf',
-#
-# )
 
-# fig['layout']['xaxis4'].update(
-#     overlaying='x1',
-#     showgrid=False,
-#     title='second curve'
-# )
-
-# fig['layout'].update(
-#     xaxis=dict(
-#         title='x1',
-#         hoverformat = '.2f',
-#         zeroline=False
-#         ),
-#     # xaxis2=dict(
-#     #         title= 'xaxis2 title',
-#     #         overlaying='x',
-#     #         side='top'
-#     # ),
-#     yaxis=dict(
-#         hoverformat = '.2f',
-#         showspikes=True,
-#         spikedash='solid',
-#         spikemode='across',
-#         spikesnap='cursor',
-#         spikethickness=1,
-#         autorange='reversed',
-#         zeroline=False,
-#         tickmode='linear',
-#         ),
-#     hovermode= 'closest',
-#     # legend=dict(orientation="h", y=1.05)
-# )
-
-# fig_out['layout'].update(
-#     xaxis=dict(
-#         hoverformat = '.2f',
-#         ),
-#     yaxis=dict(
-#         hoverformat = '.2f',
-#         showspikes=True,
-#         spikedash='solid',
-#         spikemode='across',
-#         spikesnap='cursor',
-#         spikethickness=1,
-#         zeroline=False,
-#         autorange='reversed'
-#         ),
-#     hovermode= 'closest',
-# )
-
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 
 app.layout = html.Div([
@@ -225,11 +123,11 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='algo-dropdown',
                 options=[
-                    {'label': 'Gradient Boosting', 'value': 'Gradient Boosting'},
+                    {'label': 'Gradient Boosting', 'value': 'Gradient boosting'},
                     {'label': 'Linear regression', 'value': 'Linear regression'},
                     {'label': 'Random forest', 'value': 'Random forrest'},
                 ],
-            value='Gradient Boosting'
+            value='Gradient boosting'
             ),
 
         ],
@@ -248,10 +146,6 @@ app.layout = html.Div([
             style={'height': 50000},
             id='well-plot',
         ),
-        # dcc.Graph(
-        #     figure=fig_out,
-        #     style={'height': 1900, 'width': '49%', 'display': 'inline-block'},
-        # )
     ])
 ])
 
@@ -263,12 +157,16 @@ app.layout = html.Div([
             dash.dependencies.Input(component_id='algo-dropdown',  component_property='value')
             ])
 def plot(wellname, algoname):
-    path = '../clean/Cheal-'+wellname+'_Clean.csv'
-    data = dictdata(path)
-    respath = '../Results/Cheal-'+wellname+'/'+algoname+'.csv'
-    pred_data = readres(respath)
-    col_idx = 1
+    # path = '../clean/Cheal-'+wellname+'_Clean.csv'
+    # data = dictdata(path)
+    # respath = '../Results/Cheal-'+wellname+'/'+algoname+'.csv'
+    # pred_data = readres(respath)
 
+    col_idx = 1
+    data = well_dict[wellname]['in']
+    pred_data = well_dict[wellname]['out'][algoname]
+
+    fig.data = []
     trace_num = 1
     for sbp in input_col_curves:
         for label in sbp:
@@ -279,19 +177,21 @@ def plot(wellname, algoname):
             trace_num += 1
         col_idx += 1
 
+    ylist = [d['y'] for d in fig.data]
+    ylist = np.array(ylist).flatten()
+    newl = []
+    for y in ylist:
+        for d in y:
+            newl.append(float(d))
 
-
-    # col_idx = 1
-    # for sbp in output_col_curves:
-    #     for label in sbp:
-    #         fig_out.add_trace(get_curve(output_data[label][0], output_data[label][1], label), 1, col_idx)
-    #     col_idx += 1
+    maxy = np.max(newl)
+    miny = np.min(newl)
 
     fig['layout']['xaxis2'].update(
         type='log',
-        # title='x2dgdsf',
-
     )
+
+
 
 
     fig['layout'].update(
@@ -299,11 +199,6 @@ def plot(wellname, algoname):
             hoverformat = '.2f',
             zeroline=False
             ),
-        # xaxis2=dict(
-        #         title= 'xaxis2 title',
-        #         overlaying='x',
-        #         side='top'
-        # ),
         yaxis=dict(
             hoverformat = '.2f',
             showspikes=True,
@@ -311,13 +206,39 @@ def plot(wellname, algoname):
             spikemode='across',
             spikesnap='cursor',
             spikethickness=1,
-            autorange='reversed',
+            # autorange='reversed',
             zeroline=False,
             tickmode='linear',
+            range=[maxy, miny]
             ),
+        yaxis2=dict(
+            range=[maxy, miny]
+        ),
+        yaxis3=dict(
+            range=[maxy, miny]
+        ),
+        yaxis4=dict(
+            range=[maxy, miny]
+        ),
+        yaxis5=dict(
+            range=[maxy, miny]
+        ),
+        yaxis6=dict(
+            range=[maxy, miny]
+        ),
+        yaxis7=dict(
+            range=[maxy, miny]
+        ),
+        yaxis8=dict(
+            range=[maxy, miny]
+        ),
+        yaxis9=dict(
+            range=[maxy, miny]
+        ),
+
         hovermode= 'closest',
-        # legend=dict(orientation="h", y=1.05)
     )
+    return fig
 
 
 
@@ -330,8 +251,10 @@ def get_well_score(wellname,algoname):
     respath = '../Results/Cheal-'+wellname+'/'+algoname+'_score.csv'
     df = pd.read_csv(respath)
     error = df['ERROR'][0]
-    print(error)
     return "R² score: {:2f}".format(error)
 
+app.config.supress_callback_exceptions = True
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False, threaded=True, host='0.0.0.0')
+
